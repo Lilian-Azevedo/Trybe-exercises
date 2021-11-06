945fc9eadfcf7690f5dc940facf9f09efef3a18a
Lilian <lilian.azevedo0905@gmail.com>
2021-11-06 13:33:55 -0300

    exercicio swith do 4.1

---
+++ M1-Fundamentos/b4-introducao-a-javascript-e-logica/dia-1-javascript-prmeiros-passos/script-switch.js
@@ -0,0 +1,18 @@
+let aprovacao= "lista";
+
+switch (aprovacao) {
+  case "aprovada":
+      console.log ("aprovada");
+      break;
+      
+  case "lista":
+      console.log ("lista");
+      break;
+
+  case "reprovada":
+      console.log ("reprovada");
+      break;
+
+  default:
+      console.log ("Não se aplica");
+}
