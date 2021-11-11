58ec661592d73d776a85fdca82507c7040322e89
Lilian <lilian.azevedo0905@gmail.com>
2021-11-11 11:17:10 -0300

    Exercicios parte II do bloco 4.4 - function(1 e 2)

---
+++ M1-Fundamentos/b4-introducao-a-javascript-e-logica/dia-4-objetos-e-funcoes/exercicio-parteII-4-4.js
@@ -0,0 +1,26 @@
+function verificaPalindrome (palavra) {
+    palavraInvertida = palavra.split('').reverse().join('');//achei o conteúdo no site *https://www.devmedia.com.br/forum/inverter-string-no-javascript/570951 (com a seguinte explicação de Victor Machado: Utilizando a função .split(''), eu estou criando uma array com as letras, depois utilizo a função .reverse() para inverter a posição da array e por ultimo a função join(''), para unir todos os campos em uma string novamente.)
+    if (palavraInvertida == palavra){
+        return true;
+    } else {
+        return false;
+    };
+}
+
+console.log (verificaPalindrome("arara"));
+console.log (verificaPalindrome("desenvolvimento"));
+
+function arrayMaiorValor(numeros){
+    let maiorIndice=0;
+    let maiorValor=numeros[0];
+
+    for (let index in numeros){
+        if (numeros[index]>maiorValor){
+           maiorValor = numeros[index];
+           maiorIndice = index;
+        };
+    };
+    return maiorIndice;
+};
+
+console.log (arrayMaiorValor([2, 3, 6, 7, 10, 1]));
