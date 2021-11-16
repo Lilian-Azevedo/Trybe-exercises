7038526e6f505adb37e111ae86609de8adfe0a11
Lilian <lilian.azevedo0905@gmail.com>
2021-11-16 15:22:25 -0300

    Fazendo exercicios de fixaçao 1 do b5.1

---
+++ M1-Fundamentos/b5-javascript-dom-e-web-storage/dia-1-javascript-dom-e-seletores/script-5-1.js
@@ -0,0 +1,5 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+document.getElementById("page-title").innerText = "HARRY POTTER";
+document.getElementById("subtitle").innerText = "Qual o melhor livro da franquia?";
+document.getElementById("second-paragraph").innerText = "Todo mundo sabe que é Cálice de fogo e quem discordar, me processa";
