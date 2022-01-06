c4226c264d838d468d9175c23d175a1b37d3ea7b
Lilian <lilian.azevedo0905@gmail.com>
2022-01-06 14:01:02 -0300

    Exercicios 8.5 ate o 8

---
+++ M1-Fundamentos/b8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise5-6.js
@@ -0,0 +1,14 @@
+const myList = [1, 2, 3];
+
+// escreva swap abaixo
+const swap = ([first, second, third]) => {return [third, second, first]}
+//console.log(swap(myList));
+
+//6
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+//Usei o do gabarito pq não estava entendendo o que era pedido e só resolvi entender a resposta
+const toObject = ([name, brand, year]) => ({ name, brand, year });
---
+++ M1-Fundamentos/b8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise8.js
@@ -0,0 +1,6 @@
+const greet = (user = 'usuário', typeGreeting = 'Hi') => `${typeGreeting} ${user}`;
+
+//console.log(greet('John')) // 'Hi John'
+//console.log(greet('John', 'Good morning')) // 'Good morning John'
+//console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
+//9
