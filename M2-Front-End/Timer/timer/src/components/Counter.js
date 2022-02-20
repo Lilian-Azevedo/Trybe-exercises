c3362803d1d36bfc464bd742e02a8dc2907820ad
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-02-20 11:55:15 -0300

    Adicionei os botoes de stop, cancel e coloquei os states para false/true

--- M2-Front-End/Timer/timer/src/components/Counter.js
+++ M2-Front-End/Timer/timer/src/components/Counter.js
@@ -1,4 +1,7 @@
-import React, { Component, useEffect } from 'react'
+import React, { Component } from 'react';
+import playButton from '../images/play.jpg';
+import pauseButton from '../images/pause.png';
+
 
 export default class Counter extends Component {
   state = {
@@ -8,7 +11,7 @@ export default class Counter extends Component {
    showStop: false,
    showCancel: false,
    showRetake:false,
-   stoped: false
+   stoped: false, retake: false, cancel: false,
   }
 
   updateTimer = ({target: { name }}) => {
@@ -21,17 +24,21 @@ export default class Counter extends Component {
     this.setState({ 
         showStop: false,
         showRetake: true,
-        stoped: true }); 
+        stoped: true });
   }
 
   retakeTimer = () => {
     this.setState({ 
+        retake:true,
         showRetake: false,
         showStop: true });
   }
 
   cancelTimer = () => {
-    this.setState({ 
+    this.setState({
+        cancel: true,
+        seg:0,
+        min:0,
         showPlay: true,
         showStop: false,
         showCancel: false,
@@ -51,15 +58,22 @@ export default class Counter extends Component {
     }
 
     this.timer = setInterval(() => {
-        this.setState(({seg, min}) => ({
-            seg: (seg !== 0)? seg - 1: 59,
-            min: ((seg === 0) && min > 0)? min - 1: min,
-        }))
+        if(this.state.cancel) {
+            this.setState({
+                seg: 0,
+                min: 0,  
+            })
+        } else {
+            this.setState(({seg, min}) => ({
+                seg: (seg !== 0)? seg - 1: 59,
+                min: ((seg === 0) && min > 0)? min - 1: min,
+            }))
+        }
     }, 1000);
   }
 
   componentDidUpdate(_prevProps, prevState) {
-    if(prevState.seg === 1 && prevState.min === 0 || prevState.stoped) {
+    if(prevState.seg === 1 && prevState.min === 0 || prevState.stoped || prevState.cancel) {
         clearInterval(this.timer);
     }
   }
@@ -72,13 +86,13 @@ export default class Counter extends Component {
         <h3>{min === 0 || min < 10? '0'+ min : min}:{seg === 0 || seg < 10? '0'+ seg : seg }</h3>
         <div>
             <button type='button' onClick={this.updateTimer} name='increase'>Aumentar</button>
-            <button type='button' onClick={this.updateTimer} name='decrease'>Diminuir</button>
+            <button type='button' onClick={this.updateTimer} name='decrease' disabled={ min === 0}>Diminuir</button>
         </div>
         <div>
-            {showPlay && <button type='button' onClick={this.startTimer} disabled={ min === 0}>Iniciar</button>}
-            {showStop && <button type='button' onClick={this.stopTimer} >Parar</button>}
-            {showRetake && <button type='button' onClick={this.retakeTimer} >Retomar</button>}
-            {showCancel &&  <button type='button' onClick={this.cancelTimer} >Cancelar</button>}
+            {showPlay && <button type='button' onClick={this.startTimer} disabled={ min === 0}><img src={playButton}/></button>}
+            {showStop && <button type='button' onClick={this.stopTimer} ><img src={pauseButton}/></button>}
+            {showRetake && <button type='button' onClick={this.retakeTimer} ><img src={playButton}/></button>}
+            {showCancel &&  <button type='button' onClick={this.cancelTimer} ><img src={cancelButton}/></button>}
         </div>
       </div>
     )
